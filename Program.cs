8a1da1f1479f0ed74adc907a47e39d98501bff24
alexjcd <starodub.alex@gmail.com>
2018-01-08 15:58:11 +0200

    v2.0

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitLib_Al;
+
+namespace MainApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassAlex classAlex = new ClassAlex();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+           Console.WriteLine( classAlex.GetGreetings());
+        }
+    }
+}
